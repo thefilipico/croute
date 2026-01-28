9321a25363e49430a7549036705703f8d20182b1
thefilipico <219194747+thefilipico@users.noreply.github.com>
2026-01-28 09:30:18 +0100

    Dodana početna lokacija (borna je dodao btw)

--- frontend/app.ts
+++ frontend/app.ts
@@ -11,10 +11,20 @@ document.getElementById('showRoute')?.addEventListener('click', async () => {
     const url = new URL("http://localhost:3000/route");
     url.searchParams.set("type", type);
     url.searchParams.set("maxDistance", maxDistance.toString());
-
+    if (startLat && startLon) {
+        url.searchParams.set("startLat", startLat.toString)
+        url.searchParams.set("startLon", startLon.toString)
+    }
     const res = await fetch(url.toString());
     const route: POI[] = await res.json();
 
+    let startLon : number | undefined;
+    let startLat : number | undefined;
+
+    const loc = getUserLocation();
+    if (loc) {
+        [startLat, startLon] = loc;
+    }
     map.eachLayer((layer) => { if ((layer as any)._path || (layer as any)._latlng) map.removeLayer(layer); });
 
     const latlngs: [number, number][] = route.map(poi => [poi.lat, poi.lon]);
@@ -24,3 +34,14 @@ document.getElementById('showRoute')?.addEventListener('click', async () => {
         L.marker([poi.lat, poi.lon]).addTo(map).bindPopup(poi.name);
     });
 });
+
+async function getUserLocation(): Promise<[number, number] | null> {
+    return new Promise((resolve) => {
+        if (!navigator.geolocation) return resolve(null);
+
+        navigator.geolocation.getCurrentPosition(
+            (pos) => resolve([pos.coords.latitude, pos.coords.longitude]),
+            () => resolve(null)
+        );
+    });
+}
